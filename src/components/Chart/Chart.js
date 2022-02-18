99e740b40bce40312c18ff81b770e33099c9a210
Hoang Ho Minh <hmhoang@Hoangs-MacBook-Pro.local>
2022-02-18 12:16:43 +0700

    add bar chart

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -4,13 +4,16 @@ import './Chart.css';
 import ChartBar from "./ChartBar";
 
 const Chart = (props) => {
+    const dataPointValue = props.dataPoints.map(dataPoint => dataPoint.value);
+    const totalMax = Math.max(...dataPointValue);
+
     return (
         <div className="chart">
             {props.dataPoints.map((dataPoint) => {
-                <ChartBar 
+                return <ChartBar 
                     key={dataPoint.label}
                     value={dataPoint.value}
-                    maxValue={null}
+                    maxValue={totalMax}
                     label={dataPoint.label}
                 />
             })}
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,6 +2,7 @@ import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
 import { useState } from "react";
 import ExpensesList from "./ExpensesList";
+import ExpenseChart from "./ExpenseChart";
 
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -19,6 +20,7 @@ function Expenses(props) {
         onChangeFilter={filterChangeHandler}
         selected={filteredYear}
       />
+      <ExpenseChart expenses={filteredExpenses}/>
       <ExpensesList items={filteredExpenses} />
     </div>
   );
